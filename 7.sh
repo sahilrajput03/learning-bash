4a68a4bcff5b03d4f80fdf2a927c81ab926283a4
Sahil Rajput <sahilrajput03@gmail.com>
2021-05-23 20:58:25 +0530

    Add string manipualtion exercise. ~ sahil

---
+++ 7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This file demonstrates simple string manipulation with variables.
+echo -----
+A="Sahil" 				#Note you should ALWAYS use quotes around text when assigning it to variable.
+B="${A} Rajput"  	#This is how we add text to string in bash.
+
+echo $A
+echo $B
