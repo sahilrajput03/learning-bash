292e05340ab4d98f658e481d6dbce9e92efc98aa
Sahil Rajput <sahilrajput03@gmail.com>
2025-05-14 12:37:49 +0530

    Update.

--- 3.sh
+++ 3.sh
@@ -1,7 +1,8 @@
 #/usr/bin/env bash
+# Learn:
+# 1. If you run the script via 'sudo' prefixed you would see `0` value
+#       because that denotes if the program is run with root access
+#       or not.
+# 2. If the user executing the script has root permissions by default
+#       then wihout using `sudo` the output would be `0`.
 echo $UID
-echo "If you run the script via 'sudo' prefixed you would see '0' value because
-# that denotes if the program is initiaed as root access or not."
-echo
-echo "If the user executing the script has root permissions by default then wihout
-sudo the output would be '0'"
