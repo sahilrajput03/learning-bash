cff5d4ce30fecc4526ea091b77ffae5451e2b804
Sahil Rajput <sahilrajput03@gmail.com>
2025-05-18 21:36:32 +0530

    Update.

---
+++ 10.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# -e – file exists (whether regular file, directory, etc.)
+# -f – file exists and is a regular file
+# -d – file exists and is a directory
+
+if [ -e "10.sh" ]; then
+    echo "✅File exists."
+else
+    echo "❌File does not exist."
+fi
+
+if [ -f "10.sh" ]; then
+    echo "✅File exists."
+else
+    echo "❌File does not exist."
+fi
+
+if [ -d ".github" ]; then
+    echo "✅Directory exists."
+else
+    echo "❌Directory does not exist."
+fi
