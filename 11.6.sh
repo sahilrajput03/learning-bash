02db5c004f481b156c43555f57342c25aa5b7a11
Sahil Rajput <sahilrajput03@gmail.com>
2025-06-01 17:15:22 +0530

    Update.

--- 11.6.sh
+++ 11.6.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 # & 🚀 Runing two while loops in background and killing them `ctrl+c`
-# Learn: `wait`, `pgrep`, `pkill` commands.
+# Learn: `wait`, `pgrep` (with -f), `pkill` (with -f), `osascript` Cli
+#         to show system notifications in macos.
 
-# Learn: In macos `osascript` is used to show system notifications.
-
-echo "Process ID (PID) of the current shell: $" # double $ sign [autodocs issue]
+echo "PID of the current shell: $$" # double $ sign [autodocs issue]
 
 # First task in asynchronous mode
 while true; do
@@ -11,7 +12,7 @@ while true; do
     sleep 2
 done &
 PID1=$!
-echo $PID1
+echo "PID1: $PID1"
 
 # Second task (another loop or command) in asynchronous mode
 while true; do
@@ -20,7 +21,7 @@ while true; do
     sleep 2
 done &
 PID2=$!
-echo $PID2
+echo "PID2: $PID2"
 
 # This is executed when you press ctrl+c
 trap "kill $PID1 $PID2; echo 'Killed background operations.'" EXIT
@@ -34,9 +35,26 @@ trap "kill $PID1 $PID2; echo 'Killed background operations.'" EXIT
 wait
 
 # * For manual tracking if it gets orphaned in worst case:
-# Check process in background:
-# pgrep -f a.sh
-# pgrep -f 'sh ./a.sh'
+# ps au | grep '[1]1.6'
+# apple  5284   0.0  0.0 33742688    712 s024  S+    5:04PM   0:00.02 /bin/bash ./11.6.sh
+# apple  5283   0.0  0.0 33743712    724 s024  S+    5:04PM   0:00.01 /bin/bash ./11.6.sh
+# apple  5267   0.0  0.0 33728352   1016 s024  S+    5:04PM   0:00.01 /bin/bash ./11.6.sh
 
-# Kill process via:
-# pkill -f a.sh
+# * Check process in background (all below works):
+# pgrep -f 11.6.sh
+# pgrep -f 11.6
+# pgrep -f ./11.6
+# pgrep -f 'bash ./11.6.sh'
+
+# * Kill process via:
+# pkill -f 11.6
+
+# & What does -f do in `pgrep` and `pkill` command?
+# Consider this process output from `ps au` command:
+#       apple 46985   ...   /bin/bash ./11.6.sh
+# Here:
+#       COMMAND: sh
+#       ARGUMENT: ./a.s
+#       FULL COMMAND LINE: sh ./a.s
+# * So, using pgrep -f matches against this full line (sh ./a.s), rather
+#   than just the command (sh), allowing for more precise filtering.
