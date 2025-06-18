0e752373f16f5b53aa08d5cee10b6316f2de7e79
Sahil Rajput <sahilrajput03@gmail.com>
2025-06-18 18:25:45 +0530

    Update.

--- 7.sh
+++ 7.sh
@@ -2,7 +2,7 @@
 #This file demonstrates simple string manipulation with variables.
 echo -----
 A="Sahil"       #Note you should ALWAYS use quotes around text when assigning it to variable.
-B="${A} Rajput" #This is how we add text to string in bash.
+B="${A} Rajput" #This is how we concatenate text to string in bash.
 
 echo $A
 echo $B
